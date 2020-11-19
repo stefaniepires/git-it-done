a45666d0fbe13aa9349c23e0d6e68fd640c3bc17
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-18 20:47:30 -0600

    Handle Form Submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,24 @@ var getUserRepos = function(user) {
   });
 };
 
-getUserRepos("facebook");
+
+//variables created to store a reference to the <form> element and to the <input> element
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+//to be executed upon a form submission browser event 
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  //get value from input element
+  var username = nameInputEl.value.trim();
+  
+  if(username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username!");
+  }
+};
+
+//event listener to userFormEl
+userFormEl.addEventListener("submit", formSubmitHandler);
