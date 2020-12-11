0907bf8acd3248cbb0fb7bf6815bc51726b3c4b5
steftinsley <69828366+steftinsley@users.noreply.github.com>
2020-12-11 14:29:05 -0600

    added topic buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -95,7 +95,19 @@ repoEl.appendChild(statusEl);
 }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
 
 
 
