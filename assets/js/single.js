3d501825816d1ce9317d5fe4e1e5b298b24752b1
steftinsley <69828366+steftinsley@users.noreply.github.com>
2020-12-11 14:07:40 -0600

    used data from query parameter and handled errors that can arise

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -20,10 +21,21 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+    document.location.replace("./index.html");
     }
   });
+};
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
 };
 
 var displayIssues = function(issues) {
@@ -81,4 +93,5 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
