722ac8b656affe35030969cb46802bdcf2437498
steftinsley <69828366+steftinsley@users.noreply.github.com>
2020-12-11 13:25:10 -0600

    Add an Extra Message and link to github repos if more than 30 repos exist

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,26 +1,29 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-console.log(repo);
-var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  // format the github api url
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-fetch(apiUrl).then(function(response) {
-  // request was successful
-  if (response.ok) {
-    response.json().then(function(data) {
-      displayIssues(data);
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
 
-      // check if api has paginated issues
-      if (response.headers.get("Link")) {
-        displayWarning(repo);
-      }
-    });
-  }
-  else {
-    console.log(response);
-    alert("There was a problem with your request!");
-  }
-});
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    }
+    else {
+      console.log(response);
+      alert("There was a problem with your request!");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
@@ -62,6 +65,20 @@ var displayIssues = function(issues) {
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  // create link element
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
